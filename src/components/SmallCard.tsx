a661234bace003863ec9bf7ec9a81cde2e3cdc87
Samin Yaser <saminyaserwork@gmail.com>
2023-10-20 19:48:58 +0600

    adjusted brightness

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -6,10 +6,9 @@ export default function SmallCard() {
             {data.map((item, i) => (
                 <div
                     key={i}
-                    className=""
                 >
                     <div className={"relative"}>
-                        <img src="https://picsum.photos/300/250" alt={"card-pic"} className={"object-cover"}/>
+                        <img src="https://picsum.photos/300/250" alt={"card-pic"} className={"object-cover brightness-50"}/>
                         <button
                             type="button"
                             className="px-5 py-3 text-white text-lg border rounded-full border-slate-400 backdrop-blur-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
