a7107300a6b1a86d97fab9a41b7b431f06b032bd
Samin Yaser <saminyaserwork@gmail.com>
2023-10-20 19:42:50 +0600

    added wrap, fixed svg

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -2,7 +2,7 @@ const data = ["Skin", "Hair", "Body", "Makeup", "Fragrance"];
 
 export default function SmallCard() {
     return (
-        <div className="flex flex-row items-center justify-between gap-5">
+        <div className="flex flex-row items-center justify-between flex-wrap gap-5">
             {data.map((item, i) => (
                 <div
                     key={i}
@@ -12,7 +12,7 @@ export default function SmallCard() {
                         <img src="https://picsum.photos/300/250" alt={"card-pic"} className={"object-cover"}/>
                         <button
                             type="button"
-                            className="px-3 py-1 text-white border rounded-full border-slate-400 backdrop-blur-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                            className="px-5 py-3 text-white text-lg border rounded-full border-slate-400 backdrop-blur-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
                         >
                             {item}
                         </button>
